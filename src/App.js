702e0d4892ad5356e4e1e8e8bc88d782683cfbd0
Cordell Wagendorf <dorf06@gmail.com>
2020-06-04 22:36:43 -0700

    Stopping for the night

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Text, DefaultButton } from '@fluentui/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            dice: [
+                {
+                    id: 0,
+                    maxValue: 6,
+                    currentValue: 6,
+                },
+                {
+                    id: 1,
+                    maxValue: 6,
+                    currentValue: 6,
+                },
+            ]           
+        }
+        
+    }
+    
+    render() {
+        return(
+            <>
+                <Text variant={"xLarge"}>Dice Roller</Text>
+                <Dice dice={this.state.dice}/>
+            </>
+        );
+    }
+}
+
+function Dice(props) {
+
+    function rollDice() {
+        props.dice.map(die =>
+                
+        );
+    }
+
+    return(
+        <div className="ms-Grid">
+            {props.dice.map(die =>
+                <Die key={die.id} die={die}/>
+            )}
+        </div>
+    );
+}
+
+class Die extends Component {
+
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            maxValue: this.props.die['maxValue'],
+            currentValue: this.props.die['maxValue'],
+        }
+    }
+    
+    onClickHandler () {
+        this.setState({
+            currentValue: Math.trunc((Math.random() * 1000) % this.state.maxValue) + 1
+        });
+    }
+
+    render() {
+        return(
+            <DefaultButton onClick={() => this.onClickHandler()}>{this.state.currentValue}</DefaultButton>
+        );
+    }
 }
 
 export default App;
